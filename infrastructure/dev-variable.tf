840a795d7c630d6d20f87c0dfd7343752a3e0613
markquest <oguzbelien@gmail.com>
2023-10-16 21:56:46 +0000

    created ocker containers with docker-compose

--- infrastructure/dev-variable.tf
+++ infrastructure/dev-variable.tf
@@ -1,1 +1,12 @@
-404: Not Found
+variable "mykey" {}
+variable "ami" {
+  description = "amazon linux 2023 ami"
+}
+variable "region" {}
+variable "instance_type" {}
+variable "devops_server_secgr" {}
+variable "dev-server-ports" {
+  type = list(number)
+  description = "dev-server-sec-gr-inbound-rules"
+}
+variable "devservertag" {}
